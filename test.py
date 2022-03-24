f6452a7e21573f28ea6471992a19972a5ba81bb2
ourkan95@hotmail.com <ourkan95@hotmail.com>
2022-03-24 18:52:14 +0300

    Nan Numeric değerlerin silinmesi

--- test.py
+++ test.py
@@ -8,12 +8,22 @@ import warnings
 
 #Data okuması
 def read_exc(data):
-    data = pd.read_excel(data,header = 0)   
+    
+    data = pd.read_excel(data,header = 0)  
+    
     return data
+
 #Nan değerlerin silinmesi    
-def delete_nan(data):   
-    data = data.dropna()               
+def delete_nan(data):  
+    
+    data = data.dropna()    
+           
     return data
 
-
+#NaN Numeric değerlerin silinmesi
+def delete_nanNum(data):
+    
+    data = data.apply(pd.to_numeric(), errors = 'coerence')
+    
+    return data
     
