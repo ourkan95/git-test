d93e0adadfce2b197ce753bbb00b3c1c626ef2a5
ourkan95@hotmail.com <ourkan95@hotmail.com>
2022-03-24 18:44:04 +0300

    Data okuması

--- test.py
+++ test.py
@@ -1,8 +1,21 @@
+#Kütüphaneler
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import math
 import warnings
 
+
+#Data okuması
+def read_exc(data):
+    data = pd.read_excel(data,header = 0)
+    
+    return data
+    
+
+
+
+    
+    
 
 
